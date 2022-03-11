92cdfac3bb239ab03658a191e39a3071400110e0
salva <saersalva@gmail.com>
2022-03-11 15:13:35 +0100

    kkk

--- src/components/Contact/ContactElement.jsx
+++ src/components/Contact/ContactElement.jsx
@@ -1,23 +1,23 @@
-import React,{useContext, useState} from 'react'
-import { Container, Form, Row, Button, Col ,Image} from 'react-bootstrap'
+import React, { useContext, useState } from 'react'
+import { Container, Form, Row, Button, Col, Image } from 'react-bootstrap'
 import './Contact.css'
 import lost from './lost.jpg'
-import Footer  from "../Footer/Footer"
+import Footer from "../Footer/Footer"
 import alertService from '../../services/alert.service'
-import {  useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { MessageContext } from '../../context/userMessage.context'
 
 function ContactElement() {
 
-const [contactData,setContactData]=useState({
-    text:"",
-    email:""
-})
+    const [contactData, setContactData] = useState({
+        text: "",
+        email: ""
+    })
 
-const navigate = useNavigate()
-const {setMessageInfo,setShowMessage}=useContext(MessageContext)
+    const navigate = useNavigate()
+    const { setMessageInfo, setShowMessage } = useContext(MessageContext)
 
-const { text , email}=contactData
+    const { text, email } = contactData
 
     const handleInputChange = e => {
         const { value, name } = e.target
@@ -38,42 +38,41 @@ const { text , email}=contactData
             .then(({ data }) => {
                 setShowMessage(true)
                 setMessageInfo({ title: 'Success', desc: 'our team will contact you soon' })
-             
-            })
-            .then(()=>{
                 console.log("yeahhhhhhhh")
-                navigate("/user")})
+                navigate("/")
+            })
+        
             .catch(err => console.log(err))
     }
 
 
     return (
         <>
-<Footer/>
-        <Container>
-            <Row>
-                <Col md={4} >
-                    <Image className="lost" src = { lost } alt = 'lost img'/>
+            <Footer />
+            <Container>
+                <Row>
+                    <Col md={4} >
+                        <Image className="lost" src={lost} alt='lost img' />
                     </Col>
-                <Col md={8}>
+                    <Col md={8}>
 
-                    <Form className='cont' onSubmit={handleSubmit}>
-                        <h1>Doubts? Plase Contact us¡¡</h1>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Yuor email address</Form.Label>
-                            <Form.Control type="email" placeholder="name@example.com" name="email" value={contactData.email} onChange={handleInputChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Write to Us</Form.Label>
-                            <Form.Control name="text" value={contactData.text} onChange={handleInputChange} as="textarea" rows={3} />
-                        </Form.Group>
-                        <Button className="buttonS" type="submit">
-                            Send
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+                        <Form className='cont' onSubmit={handleSubmit}>
+                            <h1>Doubts? Plase Contact us¡¡</h1>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Yuor email address</Form.Label>
+                                <Form.Control type="email" placeholder="name@example.com" name="email" value={contactData.email} onChange={handleInputChange} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Write to Us</Form.Label>
+                                <Form.Control name="text" value={contactData.text} onChange={handleInputChange} as="textarea" rows={3} />
+                            </Form.Group>
+                            <Button className="buttonS" type="submit">
+                                Send
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
